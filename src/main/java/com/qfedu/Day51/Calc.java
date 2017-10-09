0a5458ff9cf773913c385c74f790465e02f98f6d
Alex <929172465@qq.com>
2017-10-09 22:07:19 +0800

    mybitas

---
+++ src/main/java/com/qfedu/Day51/Calc.java
@@ -0,0 +1,21 @@
+package com.qfedu.Day51;
+
+public class Calc {
+	public int add(int n1,int n2){
+		return n1+n2;
+
+	}
+	public int jian(int n1,int n2){
+		return n1-n2;
+
+	}
+	public int cheng(int n1,int n2){
+		return n1*n2;
+
+	}
+	public int chu(int n1,int n2){
+		return n1/n2;
+
+	}
+
+}
